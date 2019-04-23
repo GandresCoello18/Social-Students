16d6fdb45589a764bc30067d37572f1e6adfff32
GandresCoello18 <goyeselcoca@gmail.com>
2019-04-22 23:05:31 -0500

    Updates

---
+++ precache-manifest.5dd8ff7af716c24db6e12359faa121db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aee878658997abe479609e130b3213f4",
+    "url": "/GandresCoello18/social-students/static/media/astronauta.aee87865.jpg"
+  },
+  {
+    "revision": "06dd465e5815d1f78b2d",
+    "url": "/GandresCoello18/social-students/static/js/runtime~main.2488855d.js"
+  },
+  {
+    "revision": "e04401e5318f09edb02e",
+    "url": "/GandresCoello18/social-students/static/js/main.7a08141a.chunk.js"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/GandresCoello18/social-students/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "e04401e5318f09edb02e",
+    "url": "/GandresCoello18/social-students/static/css/main.24b8d92d.chunk.css"
+  },
+  {
+    "revision": "aadc44f43f0fc9b50d3997c43574134b",
+    "url": "/GandresCoello18/social-students/index.html"
+  }
+];
